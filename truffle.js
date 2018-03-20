2cf973bf10b0cf4747e44967e98b59c81c30cc58
Promothkumar Prabhakaran <kumarpromoth@gmail.com>
2018-03-20 20:40:22 +0800

    Added truffle.js for deployment

--- truffle.js
+++ truffle.js
@@ -1,4 +1,20 @@
 module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // to customize your Truffle configuration!
+  networks: {
+    development: {
+      host: "localhost",
+      port: 8545,
+      gas:   1470150,
+      network_id: "*" // Match any network id
+    },
+     ropsten:  {
+     network_id: 3,
+     host: "localhost",
+     port:  8545,
+     gas:   4700036
+}
+  },
+   rpc: {
+ host: 'localhost',
+ post:8080
+   }
 };
